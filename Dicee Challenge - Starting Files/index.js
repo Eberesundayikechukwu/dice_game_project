e1fe8e7821e75e18ebd45d27ffce43957ac69d1e
Eberesundayikechukwu <Sundayebere23@gmail.com>
2022-05-12 13:18:47 +0100

    my first commit

---
+++ Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,37 @@
+alert("you are welcome to the dice game try your luck")
+alert("choose player");
+alert(" choose between player1 and player2")
+prompt("input player")
+var player1 = "Player 1";
+var player2 = "Player 2";
+
+
+
+function editNames() {
+    player1 = prompt("Change Player1 name");
+    player2 = prompt("Change player2 name");
+
+    document.querySelector("p.Player1")
+                    .innerHTML = player1;
+                      
+    document.querySelector("p.Player2")
+                    .innerHTML = player2;
+}
+function dice(){
+    setTimeout(function (){
+    var random1 = Math.floor(Math.random() * 6) + 1;
+    var random2 = Math.floor(Math.random() * 6) + 1;
+    document.querySelector(".img1"). setAttribute("src", "images/dice" + random1 + ".png" )
+    document.querySelector(".img2"). setAttribute("src", "images/dice" + random2 + ".png" )
+    
+    if(random1 === random2){
+        document.querySelector("h1"). innerHTML = "Draw! Draw!!";
+    }
+    else if(random1 > random2){
+        document.querySelector("h1"). innerHTML = (player1 + "Wins!");
+    }
+    else{
+        document.querySelector("h1"). innerHTML = (player2  + "Wins!");
+    }
+}  , 250);
+}
